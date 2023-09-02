d57e905b80ac7573e297985f080a5c5b07517784
Mehin Melikova <mhmelikova@gmail.com>
2023-09-03 01:17:26 +0400

    Add files via upload

---
+++ emails/WelcomeTemplate.js
@@ -0,0 +1,4 @@
+import { Html } from '@react-email/html';
+import { Text } from '@react-email/text';
+import { Section } from '@react-email/section';
+import { Container } from '@react-email/container';
