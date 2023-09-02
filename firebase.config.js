99f0959da6bc0f0ddfce2fa2ba8c5211d9c12223
Mehin Melikova <mhmelikova@gmail.com>
2023-09-03 01:17:58 +0400

    Add files via upload

---
+++ firebase.config.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBsLvi9Wyh2QgL0dOkrfFgxzei9BdwaxaI",
+    authDomain: "urex-784e0.firebaseapp.com",
+    projectId: "urex-784e0",
+    storageBucket: "urex-784e0.appspot.com",
+    messagingSenderId: "64613826986",
+    appId: "1:64613826986:web:55415bd728f7ab52bdabd3"
+};
+  
+export const app = initializeApp(firebaseConfig);
