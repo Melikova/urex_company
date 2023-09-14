1b6fdf9a3a72ae1086a2d6d0c85581e81ce9e6d5
Mehin Melikova <mhmelikova@gmail.com>
2023-09-14 09:32:15 +0400

    text top

--- components/Header/page.js
+++ components/Header/page.js
@@ -83,7 +83,7 @@ function Header() {
                 <path strokeLinecap="round" strokeLinejoin="round" d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0" />
               </svg>
             </button> */}
-            <div className="relative ml-3 mt-1 cursor-pointer">
+            <div className="relative ml-3 mt-3 cursor-pointer">
             <div className="flex items-center" onClick={handleAvatarToggle} >
               <button type="button" className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
                 <span className="absolute -inset-1.5"></span>
