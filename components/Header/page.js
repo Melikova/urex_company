88639301439b11540378284b1c9c66726b884529
Mehin Melikova <mhmelikova@gmail.com>
2023-09-14 08:57:29 +0400

    home navbar done

--- components/Header/page.js
+++ components/Header/page.js
@@ -3,6 +3,7 @@
 import React, { useState, useEffect } from 'react';
 import MobileHeader from '../MobileHeader/page';
 import Link from 'next/link';
+import { FaChevronDown } from "react-icons/fa6";
 
 // style
 import './style.scss';
@@ -59,26 +60,20 @@ function Header() {
       <div className={hamburgerMenu ? 'menu hamburgerSlide' : 'menu'}>
         <div className="container">
           <div className="navLink">
-
-            <Link href="/">
-              <p>Platform</p>
-            </Link>
-            <Link href="/">
-              <p>Features</p>
-            </Link>
-            <Link href="/">
-              <p>Who we serve</p>
-            </Link>
-            <Link href="/">
-              <p>Resources</p>
-            </Link>
+            <Link href="/"><p>Platform</p></Link>
+            <Link href="/"><p>Features</p></Link>
+            <Link href="/"><p>Who we serve</p></Link>
+            <Link href="/"><p>Resources</p></Link>
           </div>
           <div className="buttons">
-            <div className="button">
-              <p>
-                Add a listing
-              </p>
-            </div>
+            <a href="/addlisting" className="button relative">
+              <p>Add a listing</p>
+              <FaChevronDown size={12} className='text-white'/>
+              {/* <div className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabIndex="-1">
+                <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-slate-200" role="menuitem" tabIndex="-1" id="user-menu-item-0">Commercial</a>
+                <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-slate-200" role="menuitem" tabIndex="-1" id="user-menu-item-1">Residential</a>
+              </div> */}
+            </a>
             {isLogged ===true ? 
             <>
             {/* <button type="button" className="hidden lg:block relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
@@ -96,6 +91,7 @@ function Header() {
                 <img className="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
               </button>
               <div className='ms-3 text-white text-sm font-medium'>{localStorage.getItem('name')}</div>
+              <FaChevronDown size={12} className='text-white ms-2'/>
             </div>
             {avatarOn &&
             <div className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabIndex="-1">
