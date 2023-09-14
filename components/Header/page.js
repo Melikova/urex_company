48e813b184576877a3eda472439da2af443aaa27
Mehin Melikova <mhmelikova@gmail.com>
2023-09-14 09:23:37 +0400

    text done

--- components/Header/page.js
+++ components/Header/page.js
@@ -83,14 +83,14 @@ function Header() {
                 <path strokeLinecap="round" strokeLinejoin="round" d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0" />
               </svg>
             </button> */}
-            <div className="relative ml-3 mt-1 cursor-pointer">
+            <div className="relative flex items-center ml-3 mt-1 cursor-pointer">
             <div className="flex items-center" onClick={handleAvatarToggle} >
               <button type="button" className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
                 <span className="absolute -inset-1.5"></span>
                 <span className="sr-only">Open user menu</span>
                 <img className="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
               </button>
-              <div className='ms-3 text-white text-sm font-medium'>{localStorage.getItem('name')}</div>
+              <div className='ms-3 text-white text-base font-medium'>{localStorage.getItem('name')}</div>
               <FaChevronDown size={12} className='text-white ms-2'/>
             </div>
             {avatarOn &&
